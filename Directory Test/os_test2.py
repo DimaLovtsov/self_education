e48574d9ddd5509446d8cc721ce9696ca2a7e23e
DimaLovtsov <dmytrolovtsov@gmail.com>
2024-06-13 15:05:31 +0200

    add new files

---
+++ Directory Test/os_test2.py
@@ -0,0 +1,5 @@
+print("It's a test number 2.")
+
+import os
+
+print("A CWD of test2 is: ", os.getcwd()) #A CWD of test2 is:  C:\Projects VS Code\repository\first_repo\self_education
---
+++ Directory/os_test.py
@@ -0,0 +1,5 @@
+print("It's a first os file.")
+
+import os
+
+print("A CWD of test is: ", os.getcwd()) #A CWD of test is:  C:\Projects VS Code\repository\first_repo\self_education
